f0fcc21fa12c3bb48716ac8099072f65f6430830
Ekku Sipilä <ekku.eki@gmail.com>
2023-10-09 18:31:10 +0200

    Remove unused code

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -38,39 +38,6 @@ function Navigation(props: NavigationProps) {
 
   return (
     <>
-      {/* <Flex
-        justifyContent="space-between"
-        alignItems="center"
-        position="absolute"
-        width="100%"
-        zIndex="101"
-        top="4"
-        px="4"
-        {...props}
-      >
-        <Link to="/">{t("header.home")}</Link>
-        <Flex mr="-1">
-          {languages.map((lang) => (
-            <Box
-              key={lang}
-              px="1"
-              lineHeight="1"
-              borderRight="2px solid"
-              borderColor="light-orange"
-              textDecoration={lang === language ? "underline" : "none"}
-              _last={{ borderWidth: 0 }}
-            >
-              <Link
-                to={originalPath}
-                language={lang}
-                style={{ pointerEvents: lang === language ? "none" : "auto" }}
-              >
-                {lang}
-              </Link>
-            </Box>
-          ))}
-        </Flex>
-      </Flex> */}
       <Flex mr="-1" position="absolute" top="4" left="2" zIndex={1}>
         {languages.map((lang) => (
           <Box
